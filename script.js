5be77d4f3154f95583ad9a33ae6b69286187d499
Luis Reyes <luisrafaelreyescaro1@gmail.com>
2026-04-04 23:19:59 -0500

    Nav and sections added

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const navBtn = document.querySelector(".nav_btn");
 const options = document.querySelector(".options");
 
-navBtn.addEventListener("click", () => {
+navBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
     options.classList.toggle("show");
+});
+
+options.addEventListener("click", (e) => {
+    e.stopPropagation();
+});
+
+window.addEventListener("click", () => {
+    options.classList.remove("show");
 });
